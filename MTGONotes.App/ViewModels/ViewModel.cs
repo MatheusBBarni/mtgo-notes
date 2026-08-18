b690fee4217b3ed5a8a7c64727d8857fff686daf
MatheusBBarni <brehm.matheus@hotmail.com>
2026-08-18 14:08:17 -0300

    fix: inherit toolkit ObservableObject without assembly alias

--- MTGONotes.App/ViewModels/ViewModel.cs
+++ MTGONotes.App/ViewModels/ViewModel.cs
@@ -1,5 +1,3 @@
-extern alias ToolkitMvvm;
-
 namespace MTGONotes.App.ViewModels;
 
-internal abstract class ViewModel : ToolkitMvvm::CommunityToolkit.Mvvm.ComponentModel.ObservableObject;
+public abstract class ViewModel : CommunityToolkit.Mvvm.ComponentModel.ObservableObject;
